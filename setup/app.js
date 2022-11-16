75798fa167db92068a23ba2219438f7d37eb765f
meghan87 <115980448+meghan87@users.noreply.github.com>
2022-11-16 17:23:12 +0530

    final commit

--- setup/app.js
+++ setup/app.js
@@ -1,1 +1,14 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+
+const colorElem = document.querySelector(".color");
+const clickBtn = document.querySelector("#btn");
+
+clickBtn.addEventListener("click",changeColor);
+
+function changeColor(){
+    let random = Math.floor(Math.random() * colors.length);
+    document.body.style.backgroundColor = colors[random];
+    colorElem.innerText = colors[random];
+}
+
+
--- setup/hex.js
+++ setup/hex.js
@@ -1,1 +1,22 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+
+function randomNumber(){
+    return Math.floor(Math.random() * hex.length);
+}
+
+
+
+
+const colorElem = document.querySelector(".color");
+const clickBtn = document.querySelector("#btn");
+
+clickBtn.addEventListener("click",function(){
+    let hexValue = "#";
+    for(let i=0;i<6;i++){
+        hexValue += hex[randomNumber()];
+    }
+
+    document.body.style.backgroundColor = hexValue;
+    colorElem.innerText = hexValue;
+})
